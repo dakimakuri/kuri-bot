3b777305fdd5479c3a6890a1c30e711992aef3d5
jabu <jaburu@dakimakuri.com>
2019-06-03 18:40:57 -0500

    add self-assignable roles

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import * as cuddlyOctopus from './cuddlyoctopus';
 import { Mutex } from './mutex';
 import { currency } from './currency';
 import { Publisher } from './publisher';
-const cats = require('cat-ascii-faces')
+const cats = require('cat-ascii-faces');
 
 const client = new Discord.Client();
 const syrene = new Discord.Client();
@@ -17,21 +17,32 @@ const publishers = {
   'cuddly-octopus': new Publisher(env.syreneToken ? syrene : client, 'cuddly-octopus')
 };
 
+let assignableRoles = [];
+
 async function checkPublisher(name: string, delayMinutes: number, fn: any) {
   while (true) {
     // wait
     await new Promise(resolve => setTimeout(resolve, 1000 * 60 * delayMinutes));
 
     // publish embeds
-    let publisher = await publishers[name];
-    await publisher.publish(await fn(publisher.lastPublish));
+    try {
+      let publisher = await publishers[name];
+      await publisher.publish(await fn(publisher.lastPublish));
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
+
 
 client.on('ready', async () => {
   for (let key in publishers) {
     let publisher = publishers[key];
     await publisher.load();
+  }
+  try {
+    assignableRoles = await fs.readJson('data/assignableRoles.json');
+  } catch (err) {
   }
   console.log(`Logged in as ${client.user.tag}!`);
 
@@ -80,7 +91,61 @@ client.on('message', async (msg: Discord.Message) => {
   } else if (content.replace(/[^a-z]/gi, '').toLowerCase().match(/^n+y+a+h*$/)) {
     // respond to nya with cat face
     msg.channel.send(cats());
-  } else {
+  } else if (content.startsWith('kuri roles')) {
+    // manage assignable roles
+    let spl = content.split(' ');
+    if (spl.length < 3 || !admin) {
+      let response = 'Assignable roles: ';
+      let first = true;
+      for (let assignableRole of assignableRoles) {
+        if (assignableRole.guild == msg.guild.id) {
+          if (!first) {
+            response += ', ';
+          }
+          let role = msg.guild.roles.get(assignableRole.role);
+          response += `${assignableRole.command} (${role.name})`;
+          first = false;
+        }
+      }
+      await msg.channel.send(response);
+    } else {
+      let roles = msg.mentions.roles.array();
+      if (roles.length == 1) {
+        let command = spl[2];
+        let guild = msg.guild.id;
+        let role = roles[0];
+        if (command[0] != '!') {
+          await msg.channel.send('Command must start with !');
+        } else {
+          if (_.find(assignableRoles, { command, guild } as any)) {
+            await msg.channel.send('Command already exists: ' + command);
+          } else {
+            if (role.hasPermission('ADMINISTRATOR')) {
+              await msg.channel.send(`${role.name} cannot be self-assigned.`);
+            } else {
+              assignableRoles.push({ command, guild, role: role.id });
+              await fs.writeJson('data/assignableRoles.json', assignableRoles);
+              await msg.channel.send('Command ' + command + ' added to assign role ' + role.name);
+            }
+          }
+        }
+      } else {
+        let command = spl[2];
+        let guild = msg.guild.id;
+        if (command[0] != '!') {
+          await msg.channel.send('Command must start with !');
+        } else {
+          if (_.find(assignableRoles, { command, guild } as any)) {
+            _.remove(assignableRoles, { command, guild } as any);
+            await msg.channel.send('Removing role command ' + command);
+            await fs.writeJson('data/assignableRoles.json', assignableRoles);
+          } else {
+            await msg.channel.send('Command does not exist: ' + command);
+          }
+        }
+      }
+    }
+  } else if (content.startsWith('kuri subscribe') || content.startsWith('kuri unsubscribe')) {
     for (let key in publishers) {
       let publisher = publishers[key];
       if (content == `kuri subscribe ${key}`) {
@@ -92,6 +157,24 @@ client.on('message', async (msg: Discord.Message) => {
         if (admin && msg.channel instanceof Discord.TextChannel) {
           await publisher.unsubscribe(msg.channel);
           await msg.channel.send(`Unsubscribed from ${key}.`);
+        }
+      }
+    }
+  } else {
+    for (let assignableRole of assignableRoles) {
+      if (content === assignableRole.command) {
+        let role = msg.guild.roles.get(assignableRole.role);
+        if (role) {
+          let hasRole = msg.member.roles.has(assignableRole.role);
+          if (hasRole) {
+            await msg.reply(`Removing the role: ${role.name}`);
+            msg.member.removeRole(role);
+          } else {
+            await msg.reply(`Giving you the role: ${role.name}`);
+            msg.member.addRole(role);
+          }
+        } else {
+          await msg.reply(`The role for that command is is no longer available.`);
         }
       }
     }
