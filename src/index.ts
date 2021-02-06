b25bd64a9fb62587cdb686a70f6a0bac97ae797e
jabu <jaburu@dakimakuri.com>
2021-02-06 07:58:08 +0000

    add helpful emojis to shop info responses

--- src/index.ts
+++ src/index.ts
@@ -75,13 +75,13 @@ async function checkShops(message: Discord.Message, forceCheck: boolean) {
       if (flag.type === 'unknown') {
         legitMessage = false;
         if (forceCheck) {
-          const note = shop.note ? `I don't know about that specific link. ${shop.note}` : `I don't have any information about ${shop.url}.`;
+          const note = shop.note ? `❓ I don't know about that specific link. ${shop.note}` : `❓ I don't have any information about ${shop.url}.`;
           if (!messages.includes(note)) {
             messages.push(note);
           }
         }
       } else if (flag.type === 'bootlegger') {
-        const note = `The shop "${shop.name}" (${shop.url}) has been known to sell bootleg products.`;
+        const note = `❌ The shop "${shop.name}" (${shop.url}) has been known to sell bootleg products.`;
         legitMessage = false;
         if (!messages.includes(note)) {
           messages.push(note);
@@ -100,7 +100,7 @@ async function checkShops(message: Discord.Message, forceCheck: boolean) {
             links += `\n<${link.url}>`;
           }
           if (links) {
-            const note = `The shop "${shop.name}" resells at a marked up price on this storefront. You may find the same products for cheaper at:${links}`;
+            const note = `ℹ️ The shop "${shop.name}" resells at a marked up price on this storefront. You may find the same products for cheaper at:${links}`;
             if (!messages.includes(note)) {
               messages.push(note);
             }
@@ -109,7 +109,7 @@ async function checkShops(message: Discord.Message, forceCheck: boolean) {
       }
     }
     if (legitMessage && forceCheck) {
-      const note = `The shop "${shop.name}" (${shop.url}) is legitimate and does not sell bootleg products.`;
+      const note = `✅ The shop "${shop.name}" (${shop.url}) is legitimate and does not sell bootleg products.`;
       if (!messages.includes(note)) {
         messages.push(note);
       }
